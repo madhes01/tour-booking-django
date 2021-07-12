36f71a29fc4668a032e390508d39371d23952a34
Madheswaran <83126598+madhes01@users.noreply.github.com>
2021-07-12 12:57:34 +0530

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-646qv-zo63mz9@pcx^cj8ey5zecrywp0+#rt(%3j7bt9l_1$$='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'project4',
         'USER': 'postgres',
-        'PASSWORD': '1998',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
